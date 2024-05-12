08deeaed707da32b0c8b4a446d7d94eb61486715
hawoxy <hawoxy@gmail.com>
2024-05-12 14:55:29 -0400

    Initial commit

---
+++ src/main/java/net/hawoxyalt/tutorialmod/TutorialMod.java
@@ -0,0 +1,19 @@
+package net.hawoxyalt.tutorialmod;
+
+import com.sun.jna.platform.win32.NTSecApi;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
