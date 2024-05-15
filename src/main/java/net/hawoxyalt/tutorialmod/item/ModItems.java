aca31aed8af0ddfaf160cd84f13d7afc2a05fdda
hawoxy <hawoxy@gmail.com>
2024-05-14 22:50:42 -0400

    added metal detector item

--- src/main/java/net/hawoxyalt/tutorialmod/item/ModItems.java
+++ src/main/java/net/hawoxyalt/tutorialmod/item/ModItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.hawoxyalt.tutorialmod.TutorialMod;
+import net.hawoxyalt.tutorialmod.item.custom.MetalDetectorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -14,6 +15,9 @@ public class ModItems {
 
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+
+    public static final Item METAL_DETECTOR = registerItem("metal_detector",
+            new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
 
     private static void addItemsToIngredientsItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
