3edda33603afa6acff8d5a8e1c0e958949f835d3
hawoxy <hawoxy@gmail.com>
2024-05-17 22:24:59 -0400

    - added datagen (yey)

--- src/main/java/net/hawoxyalt/tutorialmod/datagen/ModLootTableProvider.java
+++ src/main/java/net/hawoxyalt/tutorialmod/datagen/ModLootTableProvider.java
@@ -1,4 +1,47 @@
 package net.hawoxyalt.tutorialmod.datagen;
 
-public class ModLootTableProvider {
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.hawoxyalt.tutorialmod.block.ModBlocks;
+import net.hawoxyalt.tutorialmod.item.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.data.server.loottable.BlockLootTableGenerator;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LeafEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+
+public class ModLootTableProvider extends FabricBlockLootTableProvider {
+    public ModLootTableProvider(FabricDataOutput dataOutput) {
+        super(dataOutput);
+    }
+
+    @Override
+    public void generate() {
+        addDrop(ModBlocks.RUBY_BLOCK);
+        addDrop(ModBlocks.RAW_RUBY_BLOCK);
+        addDrop(ModBlocks.SOUND_BLOCK);
+        addDrop(ModBlocks.RUBY_ORE, copperLikeOreDrops(ModBlocks.RUBY_ORE, ModItems.RAW_RUBY, 2.0f, 5.0f));
+        addDrop(ModBlocks.DEEPSLATE_RUBY_ORE, copperLikeOreDrops(ModBlocks.DEEPSLATE_RUBY_ORE, ModItems.RAW_RUBY, 3.0f, 6.0f));
+        addDrop(ModBlocks.END_STONE_RUBY_ORE, copperLikeOreDrops(ModBlocks.END_STONE_RUBY_ORE, ModItems.RAW_RUBY, 3.0f, 6.0f));
+        addDrop(ModBlocks.NETHER_RUBY_ORE, copperLikeOreDrops(ModBlocks.NETHER_RUBY_ORE, ModItems.RAW_RUBY, 3.0f, 6.0f));
+
+    }
+
+    public LootTable.Builder copperLikeOreDrops(Block drop, Item item, Float min, Float max) {
+        return BlockLootTableGenerator.dropsWithSilkTouch(drop, (LootPoolEntry.Builder)this.applyExplosionDecay(drop,
+                 ((LeafEntry.Builder) ItemEntry
+                         .builder(item)
+                            .apply(SetCountLootFunction
+                                    .builder(UniformLootNumberProvider
+                                            .create(min, max))))
+                         .apply(ApplyBonusLootFunction.oreDrops(Enchantments.FORTUNE))));
+    }
+
 }
--- src/main/java/net/hawoxyalt/tutorialmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/hawoxyalt/tutorialmod/datagen/ModRecipeProvider.java
@@ -1,18 +1,46 @@
 package net.hawoxyalt.tutorialmod.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-import net.minecraft.registry.RegistryWrapper;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.hawoxyalt.tutorialmod.block.ModBlocks;
+import net.hawoxyalt.tutorialmod.item.ModItems;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.List;
+import java.util.function.Consumer;
 
-public class ModRecipeProvider extends FabricTagProvider.BlockTagProvider {
-    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
-        super(output, registriesFuture);
+public class ModRecipeProvider extends FabricRecipeProvider {
+    private static List<ItemConvertible> RUBY_SMELTABLES = List.of(ModItems.RAW_RUBY,
+            ModBlocks.RUBY_ORE, ModBlocks.DEEPSLATE_RUBY_ORE, ModBlocks.NETHER_RUBY_ORE, ModBlocks.END_STONE_RUBY_ORE);
+
+    public ModRecipeProvider(FabricDataOutput output) {
+        super(output);
     }
 
     @Override
-    protected void configure(RegistryWrapper.WrapperLookup arg) {
+    public void generate(Consumer<RecipeJsonProvider> exporter) {
+        offerSmelting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
+                0.7f, 200, "ruby");
+        offerBlasting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY,
+                0.7f, 100, "ruby");
 
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS,
+                ModItems.RUBY, RecipeCategory.DECORATIONS, ModBlocks.RUBY_BLOCK);
+
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_RUBY, 1)
+                .pattern("SSS")
+                .pattern("SRS")
+                .pattern("SSS")
+                .input('S', Items.STONE)
+                .input('R', ModItems.RUBY)
+                .criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
+                .criterion(hasItem(ModItems.RUBY), conditionsFromItem(ModItems.RUBY))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.RAW_RUBY)));
     }
 }
